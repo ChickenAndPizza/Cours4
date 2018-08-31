1d9879838f075a30588be18d8c2abfc8005da21a
B533P-05\533 <alessard97@hotmail.com>
2018-08-31 15:00:28 -0400

    Player 2d + movement

---
+++ Cours4 2D/Assets/Script/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField] string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    private void ChangeLevel()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
